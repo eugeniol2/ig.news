074671929be475ba927aeb9d5967840924f16b00
Eugenio araujo <eugeniodornelesl2@gmail.com>
2021-11-12 00:37:24 -0300

    fauna-auth-done

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,7 +1,37 @@
 import styled from './styles.module.scss';
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
 export function SubscribeButton() {
+
+    const [session] = useSession();
+
+    async function handleSubscribe(){
+        if (!session){
+            signIn('github')
+            return;
+        }
+
+        try{
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs( )
+
+            await stripe.redirectToCheckout({sessionId})
+        } catch (err) {
+            alert(err.message);
+        }
+    }
+
     return (
-        <button className={styled.signInButtonContent}>
+        <button 
+        type="button"
+        className={styled.signInButtonContent}
+        onClick={handleSubscribe}
+        >
             Subscribe now
         </button>
     );
