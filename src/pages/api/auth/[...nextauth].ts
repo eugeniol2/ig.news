3fb3b1299e56096c254ee1d485ae523b20604e31
Eugenio araujo <eugeniodornelesl2@gmail.com>
2021-11-18 00:29:48 -0300

    webhooks done

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,10 @@
-import { query as q } from 'faunadb'
+import { query as q } from "faunadb";
 
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
-import { fauna } from '../../../services/fauna'
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import { fauna } from "../../../services/fauna";
 
 // import { fauna } from '../../services/fauna'
-
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -13,44 +12,32 @@ export default NextAuth({
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: 'read:user'
+      scope: "read:user",
     }),
     // ...add more providers here
   ],
   callbacks: {
     async signIn(user, account, profile) {
-      const { email } = user
+      const { email } = user;
 
       try {
         await fauna.query(
           q.If(
             q.Not(
               q.Exists(
-                q.Match(
-                  q.Index('user_by_email'),
-                  q.Casefold(user.email)
-                )
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
               )
             ),
-            q.Create(
-              q.Collection('users'),
-              { data: { email } }
-            ),
-            q.Get(
-              q.Match(
-                q.Index(
-                  q.Casefold(user.email)
-                )
-              )
-            )
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index(q.Casefold(user.email))))
           )
-        )
+        );
 
-        return true
+        return true;
       } catch (error) {
-        console.log(error)
-        return false
+        console.log(error);
+        return false;
       }
-    }
-  }
-})
+    },
+  },
+});
